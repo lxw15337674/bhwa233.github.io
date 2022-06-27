d9f973dd6cb1de1d322024a7c23ab133b6daa3f8
weiji <weiji@qunhemail.com>
2022-06-27 20:04:20 +0800

    building site 2022年06月27日 20:04:20

--- content/posts/first_post.md
+++ content/posts/first_post.md
@@ -3,4 +3,8 @@ title: "First_post"
 date: 2022-06-27T19:13:52+08:00
 draft: false
 ---
-first
+# 解决github pull/push 超时问题
+1. 查看全局代理端口
+2. 修改git配置
+git config --global http.proxy http://127.0.0.1:端口号
+git config --global https.proxy https://127.0.0.1:端口号
--- deploy.bash
+++ deploy.bash
@@ -2,19 +2,14 @@
 # 错误时终止脚本
 set -e
 
-# 删除打包文件夹
-rm -rf public
+git status
 
 # 打包。
 hugo -t LoveIt # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# 进入打包文件夹
-cd public
-
 # Add changes to git.
 
-git init
-git add -A
+git add .
 
 # Commit changes.
 msg="building site $(date)"
@@ -23,7 +18,4 @@ if [ $# -eq 1 ]; then
 fi
 git commit -m "$msg"
 
-git push origin main
-
-# 回到原文件夹
-cd ..
+# git push origin main
